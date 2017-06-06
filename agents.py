69e9ca5a7f6020a6675b02439a47fe456b64ed83
Khasan Khafizov <xozzslip@gmail.com>
2017-06-06 20:29:27 +0300

    Табы заменены пробелами

--- agents.py
+++ agents.py
@@ -4,25 +4,25 @@ import math
 
 
 class DiracAgent(Agent):
-	"""Агент, на которого действует дельта функция"""
-	def __init__(self):
-		super().__init__()
-		self.initial_orientation = V(1, 0)
+    """Агент, на которого действует дельта функция"""
+    def __init__(self):
+        super().__init__()
+        self.initial_orientation = V(1, 0)
 
-	def force(self):
-		if len(self.story.positions) == 1:
-			return V(100, 0)
-		else:
-			return V(0, 0)
+    def force(self):
+        if len(self.story.positions) == 1:
+            return V(100, 0)
+        else:
+            return V(0, 0)
 
 
 class HeavisideAgent(Agent):
-	def __init__(self):
-		super().__init__()
-		self.initial_orientation = V(1, 0)
+    def __init__(self):
+        super().__init__()
+        self.initial_orientation = V(1, 0)
 
-	def force(self):
-		if len(self.story.positions) == 1:
-			return V(0, 0)
-		else:
-			return V(1, 0)
+    def force(self):
+        if len(self.story.positions) == 1:
+            return V(0, 0)
+        else:
+            return V(1, 0)
